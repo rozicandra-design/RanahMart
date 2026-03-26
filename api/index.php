007d829fd3817e5d18d3fcf4cb905640a44f3a17
desain rozicandra <Rozicandra.last@gmail.com>
2026-03-26 15:07:14 +0700

    fix vercel config

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
 <?php
 
-require __DIR__ . "/../public/index.php";
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+// Buat folder tmp yang dibutuhkan Laravel
+$directories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions',
+    '/tmp/logs',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
+}
+
+$root = __DIR__ . '/../';
+
+require $root . 'public/index.php';
